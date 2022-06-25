c3b2a88f02ad4648f3f4ef51e3186e63b8488216
alex-4reg <alex.4reg@zoho.com>
2022-06-25 12:43:50 +0300

    добавлен метод print

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -1,5 +1,10 @@
 public class Main {
     public static void main(String[] args) {
-        System.out.println("ggggg");
+        System.out.println("qwerty!!!");
     }
+
+    public void print(String string) {
+        System.out.println(string);
+    }
+
 }
